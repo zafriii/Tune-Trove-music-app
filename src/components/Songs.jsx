0ffae6f2ec496c951cd1d1fcccb3efe773b488db
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-02-14 00:43:21 +0600

    Update Songs.jsx

--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -8,299 +8,232 @@ import { FaCirclePause , FaDownload } from "react-icons/fa6";
 
 
 function Songs() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
+  const [favoriteSongIds, setFavoriteSongIds] = useState([]);
 
+  const [currentSongIndex, setCurrentSongIndex] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState([]);
+  const [duration, setDuration] = useState(0);
 
-const [searchTerm, setSearchTerm] = useState('');
-const [searchResults, setSearchResults] = useState([]);
+  const filterItem = (typeItem) => {
+    const updatedItem = SongData.filter((curElem) => {
+      return curElem.type === typeItem;
+    });
 
+    let currTime = [...currentTime];
+    currTime.push(0);
+    setCurrentTime(currTime);
 
-const [favoriteSongIds, setFavoriteSongIds] = useState([]);
+    setSearchResults(updatedItem);
+  };
 
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-const [currentSongIndex, setCurrentSongIndex] = useState(null);
-const [isPlaying, setIsPlaying] = useState(false);
-const [currentTime, setCurrentTime] = useState(0);
-const [duration, setDuration] = useState(0);
+  useEffect(() => {
+    const results = SongData.filter((song) =>
+      song.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
 
-
-const filterItem = (typeItem) => {
-  const updatedItem = SongData.filter((curElem) => {
-    return curElem.type === typeItem
-  })
-
-  setSearchResults(updatedItem);
-  
-}
-
-
-const handleChange = (event) => {
-  setSearchTerm(event.target.value);
-};
-
-useEffect(() => {
-  const results = SongData.filter((song) =>
-    song.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-  setSearchResults(results);
-}, [searchTerm]);
-
-
-const toggleFavorite = (id) => {
-  if (favoriteSongIds.includes(id)) {
-      setFavoriteSongIds(favoriteSongIds.filter(songId => songId !== id));
-  } else {
+  const toggleFavorite = (id) => {
+    if (favoriteSongIds.includes(id)) {
+      setFavoriteSongIds(favoriteSongIds.filter((songId) => songId !== id));
+    } else {
       setFavoriteSongIds([...favoriteSongIds, id]);
-  }
-};
-
-const isFavorite = (id) => {
-  return favoriteSongIds.includes(id);
-};
-
-
-
-const audioRefs = useRef([]);
-
-const togglePlay = (index) => {
-  const audio = audioRefs.current[index];
-  const prevAudio = audioRefs.current[currentSongIndex];
-  
-  if (index === currentSongIndex && isPlaying) {
-    audio.pause();
-    setIsPlaying(false);
-  } else {
-    if (prevAudio) {
-      prevAudio.pause(); 
     }
-    audio.play();
-    setIsPlaying(true);
-    setCurrentSongIndex(index);
-    
-  }
-};
+  };
 
+  const isFavorite = (id) => {
+    return favoriteSongIds.includes(id);
+  };
 
-const updateTime = (index) => {
- 
-  if (index === currentSongIndex) {
+  const audioRefs = useRef([]);
+
+  const togglePlay = (index) => {
     const audio = audioRefs.current[index];
-    setCurrentTime(audio.currentTime);
-    setDuration(audio.duration);
-  }
-};
+    const prevAudio = audioRefs.current[currentSongIndex];
 
+    if (index === currentSongIndex && isPlaying) {
+      audio.pause();
+      setIsPlaying(false);
+    } else {
+      if (prevAudio) {
+        prevAudio.pause();
+      }
+      audio.play();
+      setIsPlaying(true);
+      setCurrentSongIndex(index);
+    }
+  };
 
-const formatTime = (time) => {
-  const minutes = Math.floor(time / 60);
-  const seconds = Math.floor(time % 60);
-  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-};
+  const updateTime = (index) => {
+    if (index === currentSongIndex) {
+      const audio = audioRefs.current[index];
 
+      const updatedTime = [...currentTime];
+      updatedTime[index] = audio.currentTime;
+      setCurrentTime(updatedTime);
+      setDuration(audio.duration);
+    }
+  };
 
-const downloadSong = (audioSrc, fileName) => {
-  const link = document.createElement('a');
-  link.href = audioSrc;
-  link.download = fileName;
-  link.click();
-};
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
 
+  const downloadSong = (audioSrc, fileName) => {
+    const link = document.createElement("a");
+    link.href = audioSrc;
+    link.download = fileName;
+    link.click();
+  };
 
-const favoriteSongs = searchResults.filter(song => favoriteSongIds.includes(song.id));
-
+  const favoriteSongs = searchResults.filter((song) =>
+    favoriteSongIds.includes(song.id)
+  );
 
   return (
     <>
-
-
-          <div className="btn-container">
-
-
-           <div className="btns"> 
-
+      <div className="btn-container">
+        <div className="btns">
           <button onClick={() => setSearchResults(SongData)}>All</button>
-          <button onClick={() => filterItem ("hindi")}>Hindi</button>
-          <button onClick={() => filterItem ("english")}>English</button>
-          <button onClick={() => filterItem ("bangla")}>Bangla</button> 
+          <button onClick={() => filterItem("hindi")}>Hindi</button>
+          <button onClick={() => filterItem("english")}>English</button>
+          <button onClick={() => filterItem("bangla")}>Bangla</button>
+        </div>
 
-          </div>
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search songs..."
+            value={searchTerm}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
 
+      <div className="heading">
+        <h1>Playlist</h1>
+        <FaHeadphones className="playlist" />
+      </div>
 
-          <div className="search">
+      <div className="song-container">
+        {searchResults.map((curElem, index) => {
+          return (
+            <div className="card-container" key={curElem.id}>
+              <div className="image-container">
+                <img src={curElem.image}></img>
+              </div>
 
-            <input type='text'
-             placeholder='Search songs...'
-             value={searchTerm}
-             onChange={handleChange} />
-                
+              <div className="song">
+                <div className="song-name">
+                  <h3>{curElem.name}</h3>
+
+                  <button
+                    className="like"
+                    onClick={() => toggleFavorite(curElem.id)}
+                  >
+                    {isFavorite(curElem.id) ? (
+                      <FaHeart />
+                    ) : (
+                      <FaHeartCirclePlus />
+                    )}
+                  </button>
+
+                  <button
+                    className="download"
+                    onClick={() => downloadSong(curElem.audio, curElem.name)}
+                  >
+                    <FaDownload />
+                  </button>
+                </div>
+
+                <div className="artist-name">
+                  <h3>{curElem.artist}</h3>
+                </div>
+
+                <div className="audio" key={curElem.id}>
+                  <audio
+                    ref={(element) => (audioRefs.current[index] = element)}
+                    src={curElem.audio}
+                    type="audio/mp3"
+                    onTimeUpdate={() => updateTime(index)}
+                    onEnded={() => setIsPlaying(false)}
+                  ></audio>
+
+                  
+                  <div className="song-range">
+                    <input
+                      type="range"
+                      min={0}
+                      max={duration}
+                      value={currentTime[index] || 0}
+                      onChange={(e) => {
+                        const time = parseFloat(e.target.value);
+                        setCurrentTime((prevTime) => {
+                          const updatedTime = [...prevTime];
+                          updatedTime[index] = time;
+                          return updatedTime;
+                        });
+                        audioRefs.current[currentSongIndex].currentTime = time;
+                      }}
+                    />
+                  </div>
+
+                  <div className="player">
+                    <button
+                      className="play-pause"
+                      onClick={() => togglePlay(index)}
+                    >
+                      {currentSongIndex === index && isPlaying ? (
+                        <FaCirclePause />
+                      ) : (
+                        <FaPlayCircle />
+                      )}
+                    </button>
+                    <span>{formatTime(currentTime[index] ?? 0)}</span> /{" "}
+                    <span>{curElem.song_duration}</span>
+                  </div>
+                </div>
+              </div>
             </div>
-
-
-          </div> 
-
-
-          <div className="heading"><h1>Playlist</h1><FaHeadphones className='playlist'/></div>
-   
-    
-    <div className="song-container">
-
-                
-
-        {             
-
-                    searchResults.map((curElem , index) => {
-
-
-                        return (
-
-
-                            <div className="card-container" key={curElem.id}>
-
-
-                                    <div className="image-container">
-
-                                         <img src={curElem.image}></img>
-
-                                    </div>
-
-                                    <div className="song">
-                                        
-                                        
-                                    <div className="song-name">
-
-                                        <h3>{curElem.name}</h3>  
-
-
-                                                                                                                   
-
-                                    <button
-                                        className='like'
-                                        onClick={() => toggleFavorite(curElem.id)}>
-                                    
-                                        {isFavorite(curElem.id) ? <FaHeart /> : <FaHeartCirclePlus />}
-                                    </button>
-
-
-                                    <button
-                                    className="download"
-                                    onClick={() => downloadSong(curElem.audio, curElem.name)}
-                                  >
-                                    <FaDownload />
-                                  </button>
-
-                                     
-
-                                    </div>  
-
-
-                                    <div className="artist-name">
-
-                                        <h3>{curElem.artist}</h3>  
-
-                                    </div>  
-
-
-                                     <div className="audio" key={curElem.id}>
-
-
-                                  
-                                <audio
-                                ref={(element) => (audioRefs.current[index] = element)}
-                                src={curElem.audio}
-                                type="audio/mp3"
-                                onTimeUpdate={() => updateTime(index)}
-                                onEnded={() => setIsPlaying(false)}
-                             
-                                ></audio>
-
-
-                              <div className="song-range">
-
-
-                              <input
-                              type="range"
-                              min={0}
-                              max={duration}
-                              value={currentTime}
-                              onChange={(e) => {
-                                const time = parseFloat(e.target.value);
-                                setCurrentTime(time);
-                                audioRefs.current[currentSongIndex].currentTime = time;
-                              }}
-                            />
-
-                                </div>
-
-
-                              <div className='player'>
-
-                                <button className='play-pause' onClick={() => togglePlay(index)}>
-                                  {currentSongIndex === index && isPlaying ? <FaCirclePause /> : <FaPlayCircle />}
-                                </button>
-                                <span>{formatTime(currentTime)}</span> /{' '}
-                                <span>{curElem.song_duration}</span>
-                                
-                              </div>
-
-                                                                                                         
-                                    </div>
-
-
-
-                                    </div>        
-                                        
-                                    </div>                            
-
-                        )
-
-                })
-                            
-        };
-
-
-    </div>
-
-
-
-                 <div className="fav-container">
-
-                      <h3>Keep Track  of Your Favorites  Here :</h3>
-
-                        {
-
-                    favoriteSongs.map((curElem , index) => {
-
-                      const serialNumber = index + 1;
-
-                      return (
-                        <div className="fav-card-container" key={curElem.id}>
-                          {/* Render favorite song card */}
-
-                         
-                          <div className="fav-image-container">
-                          
-
-                            <img src={curElem.image}></img>
-
-                            </div>
-
-                          <div className="fav-song-name">
-
-                            <p>{serialNumber}. {curElem.name}</p> 
-
-                          </div>
-
-                        </div>
-                      );
-                    })
-
-
-                        }
-                        
-                    </div>         
-    
+          );
+        })}
+        ;
+      </div>
+
+      <div className="fav-container">
+        <h3>Keep Track of Your Favorites Here :</h3>
+
+        {favoriteSongs.map((curElem, index) => {
+          const serialNumber = index + 1;
+
+          return (
+            <div className="fav-card-container" key={curElem.id}>
+              
+              <div className="fav-image-container">
+                <img src={curElem.image}></img>
+              </div>
+
+              <div className="fav-song-name">
+                <p>
+                  {serialNumber}. {curElem.name}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default Songs
+export default Songs;
